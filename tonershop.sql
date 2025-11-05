a88724160a547dfe540a36650ed911fa9b9e5af3
dankarapetian <91783477+dankarapetian@users.noreply.github.com>
2025-11-05 08:18:28 +0100

    Update and rename tonershop.db to tonershop.sql

---
+++ tonershop.sql
@@ -0,0 +1,8 @@
+create database Tonershop; 
+Use database Tonerschop;
+
+create table T_Drucker(
+Drucker_ID int auto_increment primary key,
+Drucker_name varchar(10) not null,
+Hauptuser varchar(10) not null
+);
